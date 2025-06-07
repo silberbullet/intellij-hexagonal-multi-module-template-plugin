8317f903d1c442dd7af3605bceb23d8481f0092b
silberbullet <pkw961028@naver.com>
2025-06-07 15:29:02 +0900

    fix(plugin): fix name and version

--- src/main/kotlin/com/silberbullet/views/models/common/TemplateCommonModel.kt
+++ src/main/kotlin/com/silberbullet/views/models/common/TemplateCommonModel.kt
@@ -1,9 +1,15 @@
 package com.silberbullet.views.models.common
 
-import com.intellij.openapi.application.ApplicationInfo
-import com.intellij.openapi.application.ApplicationNamesInfo
+import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.openapi.extensions.PluginId
 
-data class TemplateCommonModel (
-    val title: String = ApplicationNamesInfo.getInstance().fullProductName,
-    val version: String = ApplicationInfo.getInstance().fullVersion,
+data class TemplateCommonModel(
+    val title: String = PluginManagerCore
+        .getPlugin(PluginId.getId("io.github.silberbullet.hexagonal-multi-module-template"))
+        ?.name
+        .orEmpty(),
+    val version: String = PluginManagerCore
+        .getPlugin(PluginId.getId("io.github.silberbullet.hexagonal-multi-module-template"))
+        ?.version
+        .orEmpty()
 )
